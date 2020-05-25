e04df384e4511e28b3fcd473647f8daba4d9ab9a
Shubham Bhavsar <s3596776@student.rmit.edu.au>
2020-05-26 00:18:17 +1000

    main method

--- src/hashload.java
+++ src/hashload.java
@@ -1,6 +1,22 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class hashload {
-public static void main(String[] args) {
+public static void main(String[] args) throws IOException { 
+	int pageSize=0;
 	
+	try{
+		pageSize = Integer.parseInt(args[0]);
+	   } catch(NumberFormatException e){
+		   pageSize = 4096;
+		   System.out.println("Invalid page size given. Set to default value of 4096");
+	   }
+	 hashload map = new hashload();
+	 long startTime = System.currentTimeMillis();
+	  // map.hashFunction(pageSize);
+	 
+	 FileWriter myWriter = new FileWriter("hash."+pageSize);
+
+	 myWriter.close();
 }
 }
