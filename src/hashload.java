b5bb5ad6bafaeb574d971974bd08351cbc95e175
Shubham Bhavsar <s3596776@student.rmit.edu.au>
2020-05-26 00:22:55 +1000

    variable declaration

--- src/hashload.java
+++ src/hashload.java
@@ -2,9 +2,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class hashload {
-public static void main(String[] args) throws IOException { 
+
+	static final int RECORD_SIZE = 368;
+	   static final int BUILDINGNAME_SIZE = 65;
+	   static final int BNAME_OFFSET = 16; 
+	   static final String NODE_DELIMITER= ":::";
+	   static final String BUCKET_DELIMITER = "#";
+	   int position=-368;
+	   static int currentPositionInHeap = 0; 
+	   static boolean lastRecordFlag = false;  
+	   static int trailingWhiteSpace;
+	
+	   int hcounter=0;
+	   
+	public static void main(String[] args) throws IOException { 
 	int pageSize=0;
 	
+	 private ArrayList<HashNode> bucketArray; 
+	
+	 private int numBuckets; 
 	try{
 		pageSize = Integer.parseInt(args[0]);
 	   } catch(NumberFormatException e){
