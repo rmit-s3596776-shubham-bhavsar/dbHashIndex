754801e5e88ffde9dd301850fd14e9d63493b068
Shubham Bhavsar <s3596776@student.rmit.edu.au>
2020-05-26 15:11:08 +1000

    varialble declaration

--- src/hashquery.java
+++ src/hashquery.java
@@ -1,4 +1,11 @@
+import java.io.IOException;
 
 public class hashquery {
+	public static final int RECORD_SIZE = 368; 
+	static final String BUCKET_DELIMITER = "#";
+	static final String NODE_DELIMITER= ":::";
 
+	public static void main(String[] args) throws IOException {
 }
+}
+	
